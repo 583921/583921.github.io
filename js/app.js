a68e918dab60d349b67d4feb2b5e7cf6f02d50c8
583921 <72737126+583921@users.noreply.github.com>
2020-12-01 18:53:38 +0000

    Add files via upload
    Root folder contents

---
+++ js/app.js
@@ -0,0 +1,12 @@
+alert('Hello');
+var firstName;
+var surname;
+var name;
+firstName = prompt("Hi, what is your first name?");
+surname = prompt("Thanks! What's your surname?");
+name = firstName + " " + surname;
+console.log(name);
+
+alert(name + ", please don't get your hopes up...");
+alert("Old USB failed this morning, this is what I've managed to do today :'(")
+
